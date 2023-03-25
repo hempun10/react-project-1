4d8818d475a7434c456485e44759cb3ff5caaeab
Hem Pun <hemthegentlmen@gmail.com>
2023-03-26 00:21:03 +0545

    Done with Desktop nav

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import Billing from "./Billing";
 import CardDeal from "./CardDeal";
 import Business from "./Business";
-import Clients from "./Clients";
+import Clients from "./Client";
 import CTA from "./CTA";
 import Stats from "./Stats";
 import Footer from "./Footer";
